cc48f1ebf321ec58e3f6164bb4c91cb203250032
axel <axelguzman117@gmail.com>
2024-06-07 19:43:57 -0600

    subiendo proyecto a github

---
+++ src/app/componentes/formulario-pago/formulario-pago.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-pago',
+  templateUrl: './formulario-pago.component.html',
+  styleUrls: ['./formulario-pago.component.css']
+})
+export class FormularioPagoComponent {
+
+}
