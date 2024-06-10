33bc94bfd738e909cef2e845c45d4272f5c1c411
axel <axelguzman117@gmail.com>
2024-06-09 23:10:02 -0600

    formulario para el endpoint POST completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from './dialog.component';
+
+interface ApiResponse {
+  statusCode: number;
+  message: string;
+  transactionId: string;
+  approved: boolean;
+  errors: Record<string, string>;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -7,11 +19,14 @@ import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms'
   styleUrls: ['./app.component.css']
 })
 
+
+
 export class AppComponent {
 
   form: FormGroup;
+  showBack = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient, private dialog: MatDialog) {
     this.form = this.fb.group({
       amount: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
       email: ['', [Validators.required, Validators.email]],
@@ -24,12 +39,56 @@ export class AppComponent {
   }
 
 
+
   enviarValores() {
     if (this.form.valid) {
       console.log(this.form.value);
+      this.http.post<ApiResponse>('http://localhost:8080/api/transaccion', this.form.value)  //URL del endpoint
+        .subscribe(res => {
+          console.log(res);
+          if (res && res.message) {
+            this.dialog.open(DialogComponent, {
+              data: { message: res.message }
+            });
+          }
+        }, err => {
+          console.error(err);
+          if (err && err.error && err.error.message) {
+            this.dialog.open(DialogComponent, {
+              data: { message: 'Error en la transacción: ' + err.error.message }
+            });
+          }
+        });
     } else {
       console.log('Formulario no válido');
     }
   }
+
+  getErrorMessage(field: string): string {
+    const control = this.form.get(field);
+    if (control?.hasError('required')) {
+      return 'Este campo es requerido';
+    } else if (control?.hasError('pattern')) {
+      return 'Formato inválido';
+    } else if (control?.hasError('email')) {
+      return 'Correo electrónico inválido';
+    } else if (control?.hasError('minlength')) {
+      return `Debe tener al menos ${control.errors?.['minlength'].requiredLength} caracteres`;
+    } else if (control?.hasError('maxlength')) {
+      return `Debe tener como máximo ${control.errors?.['maxlength'].requiredLength} caracteres`;
+    }
+    return '';
+  }
+  showCardBack() {
+    this.showBack = true;
+  }
+
+  showCardFront() {
+    this.showBack = false;
+  }
+
+
+
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogComponent } from './dialog.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent,
   ],
   imports: [
     FormsModule,
@@ -18,7 +25,12 @@ import { MatInputModule } from '@angular/material/input'
     ReactiveFormsModule,
     MatInputModule,
     MatFormFieldModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule,
+    MatDialogModule,
+    MatIconModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
